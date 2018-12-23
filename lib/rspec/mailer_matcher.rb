e3571f76126ec682846faab197f00a8f21cdf7fc
Daiki Matoba <telnetstat@gmail.com>
2018-12-23 11:27:46 +0900

    Initialize gem

---
+++ lib/rspec/mailer_matcher.rb
@@ -0,0 +1,8 @@
+require "rspec/mailer_matcher/version"
+
+module Rspec
+  module MailerMatcher
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rspec/mailer_matcher_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rspec::MailerMatcher do
+  it "has a version number" do
+    expect(Rspec::MailerMatcher::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
